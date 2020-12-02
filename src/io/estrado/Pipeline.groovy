8a8a38bed736c3fbfd0e81bf75f63b5136d26003
jldeen <jedeen@outlook.com>
2020-12-02 15:32:53 -0800

    fix error

--- src/io/estrado/Pipeline.groovy
+++ src/io/estrado/Pipeline.groovy
@@ -26,8 +26,8 @@ def helmConfig() {
     //setup helm connectivity to Kubernetes API and Tiller
     println "initiliazing helm client"
     // sh "helm init --client-only"
-    // sh 'HELM_VERSION=$(helm version)'
-    // sh 'if [[ "${HELM_VERSION}" == 2* ]]; then helm init --client-only; else echo "using helm3, no need to initialize helm"; fi'
+    sh 'HELM_VERSION=$(helm version)'
+    sh 'if [[ "${HELM_VERSION}" == 2* ]]; then helm init --client-only; else echo "using helm3, no need to initialize helm"; fi'
     println "checking client/server version"
     sh "helm version"
 }
@@ -58,7 +58,8 @@ def helmDeploy(Map args) {
         println "Running deployment"
 
         sh "helm dependency update ${args.chart_dir}"
-        sh "helm upgrade --install ${args.name} ${args.chart_dir} " + (release_overrides ? "--set ${release_overrides}" : "") + "--kubeconfig string ${args.kubeconfig}" + " --namespace=${namespace}" + " --wait"
+        
+        sh "helm upgrade --install ${args.name} ${args.chart_dir} " + (release_overrides ? "--set ${release_overrides}" : "") + " --namespace=${namespace}" + " --wait"
 
         echo "Application ${args.name} successfully deployed. Use helm status ${args.name} to check"
     }
